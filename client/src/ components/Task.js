3caf7f269b3d08849018e04c0a4eff294033d119
olivia <opierez@gmail.com>
2023-02-23 13:40:07 -0500

    minor task component change to fix errors

--- client/src/ components/Task.js
+++ client/src/ components/Task.js
@@ -6,6 +6,7 @@ function Task({ task }) {
 
 
 
+
    
 
     return (
@@ -13,18 +14,18 @@ function Task({ task }) {
         //     <h2>{task.title}</h2>
         //     <h3>{task.description}</h3>
         // </div>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <div class="px-6 py-4">
-                <div class="text-base mb-2">{title}</div>
-                
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="px-6 py-4">
+                <div className="text-base mb-2">{title}</div>
+                <p>{task.completed}</p>
                 {/* <p class="text-gray-700 text-base">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                 </p> */}
             </div>
             <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
             </div>
         </div>
     )
